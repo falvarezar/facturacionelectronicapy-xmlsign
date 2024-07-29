52f8076bef7e647ab88f482a80752f091ad092f7
falvarezar <68131742+falvarezar@users.noreply.github.com>
2024-07-29 07:15:52 -0400

    actualizacion

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,74 @@
-import dsig from "./XMLDsig";
+import dsigJava from "./XMLDsigJava";
+import dsigNode from "./XMLDsigNode";
 
 class DESign {
-  signXML = (xml: string, file: any, password: any): Promise<any> => {
-    //return new Promise((resolve, reject) => {
-    dsig.openFile(file, password);
-    return dsig.signDocument(xml, "DE");
-    //});
+  signXML = (
+    xml: string,
+    file: any,
+    password: any,
+    signByNodeJS?: boolean
+  ): Promise<any> => {
+    if (signByNodeJS == true) {
+      return dsigNode.signDocument(xml, "DE", file, password);
+    } else {
+      return dsigJava.signDocument(xml, "DE", file, password);
+    }
+    //return dsigJava.signDocument(xml, "DE", file, password);
   };
-  signXMLFiles = (xmls: Array<any>, file: any, password: any): Promise<any> => {
-    //return new Promise((resolve, reject) => {
-    dsig.openFile(file, password);
-    return dsig.signDocuments(xmls, "DE");
-    //});
+
+  signXMLFiles = (
+    xmls: Array<any>,
+    file: any,
+    password: any,
+    signByNodeJS?: boolean
+  ): Promise<any> => {
+    if (signByNodeJS == true) {
+      return dsigNode.signDocuments(xmls, "DE", file, password);
+    } else {
+      return dsigJava.signDocuments(xmls, "DE", file, password);
+    }
   };
-  signXMLEvento = (xml: string, file: any, password: any): Promise<any> => {
-    //return new Promise((resolve, reject) => {
-    dsig.openFile(file, password);
-    return dsig.signEvento(xml, "DE");
-    //});
+
+  /*signXMLFilesNode = (
+    xmls: Array<any>,
+    file: any,
+    password: any
+  ): Promise<any> => {
+    //return dsigJava.signDocuments(xmls, "DE", file, password);
+    return dsigNode.signDocument(xmls, "DE", file, password);
+  };*/
+
+  signXMLEvento = (
+    xml: string,
+    file: any,
+    password: any,
+    signByNodeJS?: boolean
+  ): Promise<any> => {
+    if (signByNodeJS == true) {
+      return dsigNode.signEvento(xml, "rEve", file, password);
+    } else {
+      return dsigJava.signEvento(xml, "rEve", file, password);
+    }
+
+    //return dsigNode.signDocument([xml], "DE", file, password);
   };
-  signXMLRecibo = (xml: string, file: any, password: any): Promise<any> => {
-    //return new Promise((resolve, reject) => {
-    dsig.openFile(file, password);
-    return dsig.signDocument(xml, "recibo");
-    //});
+
+  signXMLRecibo = (
+    xml: string,
+    file: any,
+    password: any,
+    signByNodeJS?: boolean
+  ): Promise<any> => {
+    if (signByNodeJS == true) {
+      return dsigNode.signDocument(xml, "recibo", file, password);
+    } else {
+      return dsigJava.signDocument(xml, "recibo", file, password);
+    }
+    //return dsigNode.signDocument([xml], "DE", file, password);
   };
+
   getExpiration = (file: any, password: any): Promise<any> => {
-    //return new Promise((resolve, reject) => {
-    dsig.openFile(file, password);
-    return dsig.getExpiration();
-    //});
+    return dsigJava.getExpiration(file, password);
   };
 }
 
